7908670e08743c926c3161629acb4ea913d1182c
L4rry_Toru4n <larrydennis.ltoruan@gmail.com>
2024-09-19 15:22:23 +0800

    <fix: Updated DataManipulator and Graph> > [DataManipulator.ts] > - Changed Row interface properties: >   + Added prices stocks >   + Added ratio >   + Added upper and lower bounds >   + Added trigger alert > - Update DataManipulator class: >   + Changed generateRow function to return only a single Row object >   + Added property of prices stocks >   + Added property of ratio >   + Added properties of upper and lower bounds >   + Added property of timestamp >   + Added property of trigger alert > [Graph.tsx] > - Added additional table import from finos/perspective named TableData > - Updated new schema of componentDidMount function of Graph c lass component: >   + Added prices stocks >   + Added ratio >   + Added upper and lower bounds >   + Added trigger alert > - Changed the set of element attributes: >   + Removing column pivots >   + Changed columns by top_ask_price with ratio, lower_bound,  upper_bound and trigger_alert - Updated set of element attributes:   + Removed column-pivots   + Changed columns by from top_ask_price with ratio, lower and upper bounds and trigger alert   + Changed aggregates JSON returns with price_abc, price_def, ratio, upper_bound, lower_bound and trigger_alert - Updated componentDidUpdate function with table updating data with an array of generated rows type of TableData

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,54 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    
+    const price_ABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+    const price_DEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+    const ratio = price_ABC / price_DEF;
+    const upper_bound = 1 + 0.06;
+    const lower_bound = 1 - 0.09;
+
+    const timestamp_abc = serverResponds[0].timestamp;
+    const timestamp_def = serverResponds[0].timestamp;
+    
+    let timestamp_final = undefined;
+    
+    if(timestamp_abc > timestamp_def) {
+      timestamp_final = timestamp_abc;
+    }
+    else {
+      timestamp_final = timestamp_def;
+    }
+
+    let trigger_alert = undefined;
+
+    if(ratio > upper_bound || ratio < lower_bound) {
+      trigger_alert = ratio;
+    }
+    else {
+      trigger_alert = undefined;
+    }
+    
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+      price_abc: price_ABC,
+      price_def: price_DEF,
+      ratio,
+      timestamp: timestamp_final,
+      upper_bound: upper_bound,
+      lower_bound: lower_bound,
+      trigger_alert: trigger_alert
+    };
   }
 }
